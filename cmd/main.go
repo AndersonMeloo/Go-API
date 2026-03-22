5c477b16693a3fd9e97edc16c428b005fd3d9174
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-21 21:53:36 -0300

    feat(main): Inicializa Servidor HTTP

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"go-api/controller"
+	"go-api/usecase"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,11 +11,17 @@ func main() {
 	
 	server := gin.Default()
 
+	ProductUseCase := usecase.NewProductUseCase()
+
+	ProductController := controller.NewProductController(ProductUseCase)
+
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", ProductController.GetProducts)
 
 	server.Run(":8000")
 }
