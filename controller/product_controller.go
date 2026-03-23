13722b4d5cbe54774ae7b48155c44d82e8fbb304
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-22 21:13:13 -0300

    refactor(controller): remove mock e chama UseCase para GetProducts

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"go-api/model"
 	"go-api/usecase"
 	"net/http"
 
@@ -20,13 +19,10 @@ func NewProductController(usecase usecase.ProductUseCase) productController {
 
 func (p *productController) GetProducts(ctx *gin.Context) {
 
-	products := []model.Product{
-		{
-			ID: 1,
-			Name: "Product 1",
-			Price: 20,
-		},
+	products, err := p.productUseCase.GetProducts()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
 	}
 
 	ctx.JSON(http.StatusOK, products)
-} 
+}
